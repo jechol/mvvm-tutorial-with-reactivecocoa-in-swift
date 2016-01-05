3b619a22131a4606bfe5ed3799e2dc09718a607d
jechol <ryanrhie9@gmail.com>
2016-01-05 11:30:44 +0900

    Finished Your First ViewModel

---
+++ ReactiveFlickrSwift/View/RWTFlickrSearchViewController.swift
@@ -0,0 +1,28 @@
+//
+// Created by Jechol Lee on 1/5/16.
+// Copyright (c) 2016 Reactive Cocoa. All rights reserved.
+//
+
+import UIKit
+
+class RWTFlickrSearchViewController: UIViewController {
+
+  @IBOutlet var searchTextField: UITextField!
+  @IBOutlet var searchButton: UIButton!
+  @IBOutlet var loadingIndicator: UIActivityIndicatorView!
+  @IBOutlet var searchHistoryTable: UITableView!
+
+  var viewModel = RWTFlickrSearchViewModel()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    bindViewModel()
+  }
+
+  func bindViewModel() {
+    self.title = self.viewModel.title
+    self.searchTextField.text = self.viewModel.searchText
+  }
+
+}
---
+++ ReactiveFlickrSwift/ViewModel/RWTFlickrSearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+// Created by Jechol Lee on 1/5/16.
+// Copyright (c) 2016 Reactive Cocoa. All rights reserved.
+//
+
+import Foundation
+
+class RWTFlickrSearchViewModel: NSObject {
+
+  dynamic var searchText = "search text"
+  dynamic var title = "Flickr Search"
+
+}
